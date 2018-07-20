af1cfadacbc69af51da9de7d4973a51bfc3025f2
BrunoBFerreira <bruno.b.ferreira@tecnico.ulisboa.pt>
2018-07-20 18:01:53 +0100

    Added remaining classes of Test to Trilobite

--- Python/Trilobite.py
+++ Python/Trilobite.py
@@ -129,7 +129,7 @@ class Wall:
         self.p2 = p2
         self.width = width
         self.height = height
-        self.result = empty_shape()
+        self.result = None
         add_geometry(self)
         if lvls:
             for lvl in lvls:
@@ -139,37 +139,195 @@ class Wall:
                 spc.add_element(self)
 
     def generate(self):
-        v0 = self.p2 - self.p1
-        v1 = rotated_v(v0, pi / 2)
-        c = loc_from_o_vx_vy(self.p1, v0, v1)
-        self.result = box(c - vy(self.width / 2, c.cs), distance(self.p1, self.p2), self.width, self.height)
+        if self.result is None:
+            v0 = self.p2 - self.p1
+            v1 = rotated_v(v0, pi / 2)
+            c = loc_from_o_vx_vy(self.p1, v0, v1)
+            self.result = box(c - vy(self.width / 2, c.cs), distance(self.p1, self.p2), self.width, self.height)
         return self.result
 
     @around(layer_3d)
     def generate(self):
-        v0 = self.p2 - self.p1
-        v1 = rotated_v(v0, pi / 2)
-        c = loc_from_o_vx_vy(self.p1, v0, v1)
-        self.result = box(c - vy(self.width / 2, c.cs), distance(self.p1, self.p2), self.width, self.height)
+        if self.result is None:
+            v0 = self.p2 - self.p1
+            v1 = rotated_v(v0, pi / 2)
+            c = loc_from_o_vx_vy(self.p1, v0, v1)
+            self.result = box(c - vy(self.width / 2, c.cs), distance(self.p1, self.p2), self.width, self.height)
         return self.result
 
     @around(layer_2d)
     def generate(self):
-        v0 = self.p2 - self.p1
-        v1 = rotated_v(v0, pi / 2)
-        c = loc_from_o_vx_vy(self.p1, v0, v1)
-        self.result = rectangle(c - vy(self.width / 2, c.cs), distance(self.p1, self.p2), self.width)
+        if self.result is None:
+            v0 = self.p2 - self.p1
+            v1 = rotated_v(v0, pi / 2)
+            c = loc_from_o_vx_vy(self.p1, v0, v1)
+            self.result = rectangle(c - vy(self.width / 2, c.cs), distance(self.p1, self.p2), self.width)
         return self.result
 
     @around(layer_analysis)
     def generate(self):
-        v0 = self.p2 - self.p1
-        v1 = rotated_v(v0, pi / 2)
-        c = loc_from_o_vx_vy(self.p1, v0, v1)
-        p2 = c + vx(distance(self.p1, self.p2), c.cs)
-        p3 = p2 + vz(self.height)
-        p4 = p3 - vx(distance(self.p1, self.p2), c.cs)
-        return surface_from(line(c, p2, p3, p4, c))
+        if self.result is None:
+            v0 = self.p2 - self.p1
+            v1 = rotated_v(v0, pi / 2)
+            c = loc_from_o_vx_vy(self.p1, v0, v1)
+            p2 = c + vx(distance(self.p1, self.p2), c.cs)
+            p3 = p2 + vz(self.height)
+            p4 = p3 - vx(distance(self.p1, self.p2), c.cs)
+            self.result = surface_from(line(c, p2, p3, p4, c))
+        return self.result
+
+
+class Slab:
+    def __init__(self, path, thickness=0.2, lvls=[], spcs=[]):
+        self.path = path
+        self.thickness = thickness
+        self.result = None
+        add_geometry(self)
+        if lvls:
+            for lvl in lvls:
+                lvl.add_element(self)
+        if spaces:
+            for spc in spcs:
+                spc.add_element(self)
+
+    def generate(self):
+        if self.result is None:
+            self.result = extrusion(surface_from(line(self.path)), self.thickness)
+        return self.result
+
+    @around(layer_3d)
+    def generate(self):
+        if self.result is None:
+            self.result = extrusion(surface_from(line(self.path)), self.thickness)
+        return self.result
+
+    @around(layer_2d)
+    def generate(self):
+        if self.result is None:
+            self.result = line(self.path)
+        return self.result
+
+    @around(layer_analysis)
+    def generate(self):
+        if self.result is None:
+            self.result = surface_from(line(self.path))
+        return self.result
+
+
+class SlabWithOpening:
+    def __init__(self, path, openings, thickness=0.2, lvls=[], spcs=[]):
+        self.path = path
+        self.thickness = thickness
+        self.openings = openings
+        self.result = None
+        if lvls:
+            for lvl in lvls:
+                lvl.add_element(self)
+        if spaces:
+            for spc in spcs:
+                spc.add_element(self)
+
+    def generate(self):
+        if self.result is None:
+            result = extrusion(surface_from(line(self.path)), self.thickness)
+            for lns in self.openings:
+                result = subtraction(result,
+                                     extrusion(surface_from(line(lns)), 2 * self.thickness))
+            self.result = result
+        return self.result
+
+    @around(layer_3d)
+    def generate(self):
+        if self.result is None:
+            result = extrusion(surface_from(line(self.path)), self.thickness)
+            for lns in self.openings:
+                result = subtraction(result,
+                                     extrusion(surface_from(line(lns)), 2 * self.thickness))
+            self.result = result
+        return self.result
+
+    @around(layer_2d)
+    def generate(self):
+        if self.result is None:
+            self.result = line(self.path)
+        return self.result
+
+    @around(layer_analysis)
+    def generate(self):
+        if self.result is None:
+            result = surface_from(line(self.path))
+            for lns in self.openings:
+                result = subtraction(result,
+                                     surface_from(line(lns)))
+            self.result = result
+        return self.result
+
+
+class Door:
+    def __init__(self, w, p1, p2, h, lvls=[], spcs=[]):
+        self.p1 = p1
+        self.p2 = p2
+        self.height = h
+        self.w = w
+        self.result = None
+        if lvls:
+            for lvl in lvls:
+                lvl.add_element(self)
+        if spaces:
+            for spc in spcs:
+                spc.add_element(self)
+
+    def generate(self):
+        if self.result is None:
+            v0 = self.p2 - self.p1
+            v1 = rotated_v(v0, pi / 2)
+            c = loc_from_o_vx_vy(self.p1, v0, v1)
+            if self.w.result is None:
+                self.w.generate()
+            self.w.result = subtraction(self.w.result, box(c - vy((self.w.width / 2) + 0.01, c.cs),
+                                                           distance(self.p2, self.p1),
+                                                           self.w.width + 0.01,
+                                                           self.height))
+            self.result = self.w.result # It is the same now. Update when the doors have an actual shape to be added
+        return self.result
+
+    @around(layer_3d)
+    def generate(self):
+        if self.result is None:
+            v0 = self.p2 - self.p1
+            v1 = rotated_v(v0, pi / 2)
+            c = loc_from_o_vx_vy(self.p1, v0, v1)
+            if self.w.result is None:
+                self.w.generate()
+            self.w.result = subtraction(self.w.result, box(c - vy((self.w.width / 2) + 0.01, c.cs),
+                                                           distance(self.p2, self.p1),
+                                                           self.w.width + 0.01,
+                                                           self.height))
+            self.result = self.w.result  # It is the same now. Update when the doors have an actual shape to be added
+        return self.result
+
+    @around(layer_2d)
+    def generate(self):
+        """nrho = pol_rho(self.pos1)/cos(abs(pol_phi(self.pos2) - pol_phi(self.pos1)))
+        aux1 = pol(nrho, pol_phi(self.pos2))
+        line(self.pos1, aux1)
+        arc(self.pos1, distance(self.pos1, aux1), pol_phi(self.pos1), pol_phi(self.pos1) + pi/2)"""
+        pass
+
+    @around(layer_analysis)
+    def generate(self):
+        if self.result is None:
+            v0 = self.p2 - self.p1
+            v1 = rotated_v(v0, pi / 2)
+            c = loc_from_o_vx_vy(self.p1, v0, v1)
+            p2 = c + vx(distance(self.p1, self.p2), c.cs)
+            p3 = p2 + vz(self.height)
+            p4 = p3 - vx(distance(self.p1, self.p2), c.cs)
+            if self.w.result is None:
+                self.w.generate()
+            self.w.result = subtraction(self.w.result, surface_from(line(c, p2, p3, p4, c)))
+            self.result = self.w.result  # Check comment in the other version of generate
+        return self.result
 
 
 levels[0] = Level(0, "Level 0")
